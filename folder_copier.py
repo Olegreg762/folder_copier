b41bd60170b11cdf176664bcf3bbe2442b5acf43
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-11-12 22:37:58 -0600

    removed +1 n print f string to correct copy count

--- folder_copier.py
+++ folder_copier.py
@@ -11,9 +11,9 @@ def copy_folders(source_folder, target_folder, folder_name):
                 shutil.rmtree(target_path)
             shutil.copytree(source_path, target_path)
             count += 1
-    print(f'{count+1} Folders copied')
+    print(f'{count} Folders copied')
 
 # Replace 'source_path' with the folder to searched
 # Replace 'target_path' with the folder to be copied into
 # Replace 'folder_name' with the name of the folders needing to be copied
-copy_folders('source_path', 'target_path', 'folder_name',)
+copy_folders('source_path', 'target_path', 'folder_name')
